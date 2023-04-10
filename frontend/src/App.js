a3999b8b3f58772f553d0a11e9de3ad1cc20f420
Ashutosh Kumar Shubham <ashutoshshubham29@gmail.com>
2023-04-10 12:27:52 +0530

    .

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from './components/main/Home'
+import SignIn from './components/main/SignIn'
+import SignUp from './components/main/SignUp'
+import Feedback from './components/main/Feedback'
+import ForgetPswd from './components/main/ForgetPswd'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route element={<Navigate to='/homepage' />} path='/'/>
+          <Route element={<Home />} path='homepage'/> 
+          <Route element={<SignIn />} path='signin'/> 
+          <Route element={<SignUp />} path='signup'/> 
+          <Route element={<Feedback />} path='feedback'/> 
+          <Route element={<ForgetPswd />} path='forgetpswd'/> 
+
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
